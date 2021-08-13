3a4ca08b0cb268a2288d5d96a7638744ab109953
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-08-13 14:33:33 +0200

    Fix app name in apps.py

--- devfolio/portfolio/apps.py
+++ devfolio/portfolio/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PortfolioConfig(AppConfig):
-    name = "portfolio"
+    name = "devfolio.portfolio"
+    verbose_name = _("Portfolio")
