42942e3cc2566de513383c485d49c8392ba392d5
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-08-23 13:20:44 +0200

    Remove 'About' page'

--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", include("devfolio.portfolio.urls", namespace="home")),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
@@ -46,4 +40,6 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [
+            path("__debug__/", include(debug_toolbar.urls))
+        ] + urlpatterns
