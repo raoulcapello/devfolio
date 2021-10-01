83fe9cb066b231c0e9d8903834fb34e9e503fbea
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-10-01 13:35:59 +0200

    Simplify main menu and adjust behavior for mobile

--- devfolio/static/js/devfolio.js
+++ devfolio/static/js/devfolio.js
@@ -1,1 +1,10 @@
 /* DevFolio specific Javascript */
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", () => {
+    setTimeout(() => {
+      document
+        .querySelector("#navbarCollapseMainMenu")
+        .classList.remove("show");
+    }, 500);
+  });
+});
